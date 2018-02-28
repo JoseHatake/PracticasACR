11914895a2ba782a9a7a5ed25c553b9b50c2805b
JoseMiguelGutierrezGuevara <di_tutticolori@hotmail.com>
2018-02-27 18:05:20 -0600

    Problema de tarea de un solloo semaforo casi termiando

--- P2/PCcompleto.py
+++ P2/PCcompleto.py
@@ -3,31 +3,24 @@ import threading
 s = threading.Semaphore(1)
 
 def consumidor():
-	counter = 0
-	while counter < 20:
-		s.acquire()
-		contador[0] -= 1
-		counter += 1
-		print "Valor actual: ",contador[0]
-		s.release()
+	s.acquire()
+	print "Consumidor - Valor actual: ",contador[0]
+	s.release()
 
 def productor():
-	counter = 0
-	while counter < 20:
-		s.acquire()
-		contador[0] += 1
-		counter += 1
-		print "Valor actual: ",contador[0]
-		s.release()
+	s.acquire()
+	contador[0] += 1
+	print "Productor - Valor actual: ",contador[0]
+	s.release()
 
 contador = [1]
-numeroDeIteraciones = 20
 
-hiloProductor = threading.Thread(target=productor)
-hiloConsumidor = threading.Thread(target=consumidor)
+for x in xrange(1,20):
+	hiloProductor = threading.Thread(target=productor)
+	hiloConsumidor = threading.Thread(target=consumidor)
 
-hiloConsumidor.start()
-hiloProductor.start()
+	hiloConsumidor.start()
+	hiloProductor.start()
 
-hiloConsumidor.join()
-hiloProductor.join()
+	hiloConsumidor.join()
+	hiloProductor.join()
