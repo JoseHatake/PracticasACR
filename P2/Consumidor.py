2fa0ef8bf65379ea4855c60c68a41c7adb95a823
JoseMiguelGutierrezGuevara <di_tutticolori@hotmail.com>
2018-03-08 18:33:21 -0600

    Prueba para guardar archivos

--- P2/Consumidor.py
+++ P2/Consumidor.py
@@ -1,1 +1,15 @@
-import threading
+import os.path as path
+
+file = "Resultado.txt"
+cadena = "Esta es la cadena que tiene que guardar el archivo"
+
+if path.exists(file):
+	print "Existe"
+	f = open(file,"a")
+	f.write(cadena)
+	f.close()
+else:
+	print "No existe"
+	f = open(file,"w")
+	f.write(cadena)
+	f.close()
