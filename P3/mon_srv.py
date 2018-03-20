52c5b572b9687ac6139da2438670fd07aa67d8e2
JoseMiguelGutierrezGuevara <di_tutticolori@hotmail.com>
2018-03-20 01:52:11 -0600

    Ya quedo el server

---
+++ P3/mon_srv.py
@@ -0,0 +1,111 @@
+import socket
+import asyncio
+import cmd, sys
+import threading as thread
+from queue import Queue
+
+conexiones = []
+locks = []
+
+LOK = asyncio.Semaphore(1)
+
+class ServerShell(cmd.Cmd):
+	intro = 'Server Ready, type help for a list of permitted commands.\n'
+	prompt = 'SERVER>>'
+	file = None
+
+	def do_list(self, arg):
+		'A list of current connected clients: LIST'
+		for conn in conexiones:
+			print('Conn: '+str(conexiones.index(conn))+' '+str(conn.getsockname())+'\n')
+		
+	def do_show(self, arg):
+		'Shows client monitor: SHOW 10'
+		sel = int(arg.split(' ')[0])
+		if(sel >= len(locks)):
+			print('Numero de clientes conectados:', sel)
+		else:
+			locks[sel].put(0)
+		
+	def do_stop(self, arg):
+		'Stops client monitor: STOP 10'
+		sel = int(arg.split(' ')[0])
+		if(sel >= len(locks)):
+			print('Numero de clientes conectados:', sel)
+		else:
+			locks[sel].put(1)
+		
+	def do_exit(self, arg):
+		'Ends client connection: END 10'
+		sel = int(arg.split(' ')[0])
+		if(sel >= len(locks)):
+			print('Numero de clientes conectados:', sel)
+		else:
+			conexiones.remove(conn)
+			locks.remove(lock)
+			conn.close()
+
+
+def console():
+	ServerShell().cmdloop();
+
+def servicio(conn,client_address,colaFlag):
+	try:
+		print ("Conexion desde", client_address)
+		flag = True
+
+		# Recibe los datos en trozos y reetransmite
+		while flag:
+			num = locks[0].get()
+			while True:
+				if(num == 0):
+					recv = conn.recv(1024)
+					data = recv.decode('utf-8')
+					if(len(data) > 0):
+						if locks[0].empty():
+							print ("Cliente: " + str(client_address[0]) + " Recibido: " + data)
+					else:
+						flag = False
+						break
+				else:
+					break
+	finally:
+		# Cerrando conexion
+		conexiones.remove(conn)
+		locks.remove(colaFlag)
+		conn.close()
+
+#Crea thread de interaccion
+t = thread.Thread(target=console)
+t.start()
+		
+# Creando un socket TCP/IP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Conecta el socket en el puerto cuando el servidor esté escuchando
+server_address = ('localhost', 10000)
+print ("Empezando a levantar %s puerto %s" % server_address)
+sock.bind(server_address)
+sock.listen(1)
+
+while True:
+	# Esperando conexion
+	print ("Esperando para conectarse")
+	conn, addr = sock.accept()
+	#Agregar conexion a lista de conexiones vigentes
+	conexiones.append(conn)
+	#Agregar nuevo candado por cliente conectado
+	cola = Queue()
+	locks.append(cola)
+	
+	t = thread.Thread(target=servicio,args=[conn,addr,cola])
+	t.start()
+	
+	
+	
+	
+	
+	
+	
+	
+	
